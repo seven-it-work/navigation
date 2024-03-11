bb5850ea477f8685bc3763473d9a3180fe71b0a3
seven.it.work <yijialuo@huawei.com>
2024-03-11 22:10:05 +0800

    auto push

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ const open=ref(true)
   <a-config-provider :locale="locale">
     <a-modal v-model:open="open" title="每日看板" @ok="()=>open=false"
              width="100%"
-             height="50%"
+             height="100%"
              wrap-class-name="full-modal" >
       <Gold></Gold>
     </a-modal>
@@ -24,21 +24,21 @@ const open=ref(true)
   </a-config-provider>
 </template>
 <style type="lass">
-.full-modal {
-  .ant-modal {
+.full-modal .ant-modal{
     max-width: 100%;
     top: 0;
     padding-bottom: 0;
     margin: 0;
   }
-  .ant-modal-content {
+.full-modal  .ant-modal-content {
     display: flex;
     flex-direction: column;
     height: calc(100vh);
+  overflow-y: scroll;
+  overflow-x: scroll;
   }
-  .ant-modal-body {
+.full-modal  .ant-modal-body {
     flex: 1;
   }
-}
 </style>
 
--- src/v2/comp/Gold.vue
+++ src/v2/comp/Gold.vue
@@ -5,161 +5,150 @@ import axios from "axios";
 import dayjs from "dayjs";
 
 
-
 export default {
   name: "Gold",
-  data(){
+  data() {
     return {
-      jisuapiGold:[],
-      ccbListShow:false,
-      ccbList:[],
+      ccbListShow: false,
+      ccbList: [],
     }
   },
   mounted() {
-    console.log(goldData)
-      this.ccbListShow=true
-      this.ccbList=goldData
-      setTimeout(()=>{
-        const chartDom = document.getElementById('ccbListShow');
-        const myChart = echarts.init(chartDom);
-        const option = {
-          title: {
-            text: '每日黄金'
+    this.ccbListShow = true
+    this.ccbList = goldData
+    setTimeout(() => {
+      const chartDom = document.getElementById('ccbListShow');
+      const myChart = echarts.init(chartDom);
+      const option = {
+        title: {
+          text: '每日黄金'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['开盘', '最高', '最低', '收盘', '涨幅', '五天平均值', '十天平均值']
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: this.ccbList.map(item => item.time)
+        },
+        yAxis: {
+          type: 'value',
+          min: 300
+        },
+        series: [
+          {
+            name: '开盘',
+            type: 'line',
+            smooth: true,
+            data: this.ccbList.map(item => item.open)
           },
-          tooltip: {
-            trigger: 'axis'
+          {
+            name: '最高',
+            type: 'line',
+            smooth: true,
+            data: this.ccbList.map(item => item.high)
           },
-          legend: {
-            data: ['开盘', '最高', '最低', '收盘', '涨幅', '五天平均值', '十天平均值']
+          {
+            name: '最低',
+            type: 'line',
+            smooth: true,
+            data: this.ccbList.map(item => item.low)
           },
-          toolbox: {
-            feature: {
-              saveAsImage: {}
-            }
+          {
+            name: '收盘',
+            type: 'line',
+            smooth: true,
+            data: this.ccbList.map(item => item.close)
           },
-          xAxis: {
-            type: 'category',
-            boundaryGap: false,
-            data: this.ccbList.map(item => item.time)
+          {
+            name: '涨幅',
+            type: 'line',
+            smooth: true,
+            data: this.ccbList.map(item => item.valueS)
           },
-          yAxis: {
-            type: 'value',
-            min:300
+          {
+            name: '五天平均值',
+            type: 'line',
+            smooth: true,
+            data: this.ccbList.map(item => item.FIVEDAYAVG)
           },
-          series: [
-            {
-              name: '开盘',
-              type: 'line',
-              smooth: true,
-              data: this.ccbList.map(item => item.open)
-            },
-            {
-              name: '最高',
-              type: 'line',
-              smooth: true,
-              data: this.ccbList.map(item => item.high)
-            },
-            {
-              name: '最低',
-              type: 'line',
-              smooth: true,
-              data: this.ccbList.map(item => item.low)
-            },
-            {
-              name: '收盘',
-              type: 'line',
-              smooth: true,
-              data: this.ccbList.map(item => item.close)
-            },
-            {
-              name: '涨幅',
-              type: 'line',
-              smooth: true,
-              data: this.ccbList.map(item => item.valueS)
-            },
-            {
-              name: '五天平均值',
-              type: 'line',
-              smooth: true,
-              data: this.ccbList.map(item => item.FIVEDAYAVG)
-            },
-            {
-              name: '十天平均值',
-              type: 'line',
-              smooth: true,
-              data: this.ccbList.map(item => item.TENDAYAVG)
-            },
-          ]
-        };
+          {
+            name: '十天平均值',
+            type: 'line',
+            smooth: true,
+            data: this.ccbList.map(item => item.TENDAYAVG)
+          },
+        ]
+      };
 
-        option && myChart.setOption(option);
+      option && myChart.setOption(option);
 
 
-        let zf = document.getElementById('ccbListZFShow');
-        const zfMyChart = echarts.init(zf);
-        const optionZf = {
-          title: {
-            text: '每日黄金涨幅'
-          },
-          tooltip: {
-            trigger: 'axis'
-          },
-          legend: {
-            data: [ '涨幅']
+      let zf = document.getElementById('ccbListZFShow');
+      const zfMyChart = echarts.init(zf);
+      const optionZf = {
+        title: {
+          text: '每日黄金涨幅'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['涨幅']
+        },
+
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: this.ccbList.map(item => item.time)
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: '涨幅',
+            type: 'line',
+            smooth: true,
+            data: this.ccbList.map(item => item.valueS)
           },
 
-          toolbox: {
-            feature: {
-              saveAsImage: {}
-            }
-          },
-          xAxis: {
-            type: 'category',
-            boundaryGap: false,
-            data: this.ccbList.map(item => item.time)
-          },
-          yAxis: {
-            type: 'value'
-          },
-          series: [
-            {
-              name: '涨幅',
-              type: 'line',
-              smooth: true,
-              data: this.ccbList.map(item => item.valueS)
-            },
+        ]
+      };
 
-          ]
-        };
-
-        optionZf && zfMyChart.setOption(optionZf);
-      },1000)
+      optionZf && zfMyChart.setOption(optionZf);
+    }, 1000)
   }
 }
 </script>
 
 <template>
-  <div v-if="jisuapiGold && jisuapiGold.length>0">
-    <div v-for="item in jisuapiGold" :key="item.typename">
-      <span style="margin-right: 10px">{{item.typename}}</span>
-      <span style="margin-right: 10px">{{item.midprice}}</span>
-      <span style="margin-right: 10px">{{item.buyprice}}</span>
-      <span style="margin-right: 10px">{{item.sellprice}}</span>
-      <span style="margin-right: 10px">{{item.maxprice}}</span>
-      <span style="margin-right: 10px">{{item.minprice}}</span>
-    </div>
-  </div>
   <div v-if="ccbListShow">
     <div>
-      <span style="margin-right: 10px">开盘：{{ccbList[ccbList.length-1].open}}</span>
-      <span style="margin-right: 10px">收盘：{{ccbList[ccbList.length-1].close}}</span>
-      <span style="margin-right: 10px">涨幅：{{ccbList[ccbList.length-1].valueS}}%</span>
+      <span style="margin-right: 10px"><a href="http://www3.ccb.com/chn/home/gold_new/hqzs/index.shtml" target="_blank">黄金积存报价</a></span>
+      <span style="margin-right: 10px">时间：{{ ccbList[ccbList.length - 1].time }}</span>
+      <span style="margin-right: 10px">开盘：{{ ccbList[ccbList.length - 1].open }}</span>
+      <span style="margin-right: 10px">收盘：{{ ccbList[ccbList.length - 1].close }}</span>
+      <span style="margin-right: 10px">涨幅：{{ ccbList[ccbList.length - 1].valueS }}%</span>
     </div>
     <a-row>
-      <a-col :span="12">
+      <a-col :xs="{ span: 24}" :md="{span:24}" :lg="{span:12}" :xl="{span:12}" :xxl="{ span: 12 }">
         <div id="ccbListShow"></div>
       </a-col>
-      <a-col :span="12">
+      <a-col :xs="{ span: 24 }" :md="{span:24}" :lg="{span:12}" :xl="{span:12}" :xxl="{ span: 12 }">
         <div id="ccbListZFShow"></div>
       </a-col>
     </a-row>
@@ -171,6 +160,7 @@ export default {
   width: 100%;
   height: 500px;
 }
+
 #ccbListZFShow {
   width: 100%;
   height: 500px;
